559129718c5ec19b5dce02495e1f6e6e74a6b757
gilgan9852 <gilgan9852@gmail.com>
2023-05-31 15:47:26 +0900

    db 더미 이동 및 정리

--- js/customerDB.js
+++ js/customerDB.js
@@ -10,14 +10,8 @@ const addcustomer = (id, name, mail, phone, reg_date) => {
         c_name: name,
         c_mail: mail,
         c_phone: phone,
-        c_reg_date: reg_date,
+        // c_reg_date: reg_date,
     });
 
     console.log(customerDB.get(id));
 }
-
-addcustomer('gildong','홍길동','gildong@gamil.com','010-1234-5678');
-addcustomer('chango','박찬호','chanho@naver.com','010-9012-3456');
-addcustomer('saeri','박세리','saeri@gdanm.net','010-7890-1234');
-addcustomer('byeonggyu','이병규','byeonggyu@gamil.com','010-5678-9012');
-addcustomer('heungmin','손흥민','heungmin@naver.com','010-3456-7890');
--- js/productDB.js
+++ js/productDB.js
@@ -5,20 +5,15 @@ const addproduct = (pno, mno, name, price, quantity) => {
     console.log('addUser() CALLED!!');
 
     //  상품번호(no)를 가지고 등록한다.
-    productDB.set(no, {
+    productDB.set(pno, {
         p_no: pno,
         m_no: mno,
         p_name: name,
         p_mail: price,
         p_quantity: quantity,
-        p_reg_date: reg_date,
+        // p_reg_date: reg_date,
     });
 
-    console.log(productDB.get(no));
+    console.log(productDB.get(pno));
 }
 
-addproduct('1','1','A상품','10000','50');
-addproduct('2','2','B상품','500','100');
-addproduct('3','3','C상품','15000','20');
-addproduct('4','4','D상품','20000','10');
-addproduct('5','5','E상품','30000','30');
